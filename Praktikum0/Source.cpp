69887fad7cdc412b1b0aa3cced3d813b968572d1
faizaaulia <faizaaulia20@gmail.com>
2017-08-11 13:24:15 +0700

    persegi

--- Praktikum0/Source.cpp
+++ Praktikum0/Source.cpp
@@ -4,6 +4,11 @@
 
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPointSize(50);
+	glBegin(GL_POINTS);
+	glColor3f(1, 0, 1);
+	glVertex3f(100, 200, 0);
+	glEnd();
 	glutSwapBuffers();
 }
 
@@ -11,9 +16,9 @@ int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowSize(800, 600);
-	glutCreateWindow("Praktikum0");
+	glutCreateWindow("XIIRPL6-14-Faiza Aulia Rahma Putra");
 	gluOrtho2D(0, 800, 0, 600);
-	glClearColor(0, 0, 0, 0);
+	glClearColor(1,1,1,0);
 	glutDisplayFunc(render);
 
 	glutMainLoop();
